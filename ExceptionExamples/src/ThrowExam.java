ce1c53b294741de4e234ac2d366e62c7cb6c3ccf
hjy_net <hjy_net@naver.com>
2018-05-25 09:54:29 +0900

    .

--- ExceptionExamples/src/ThrowExam.java
+++ ExceptionExamples/src/ThrowExam.java
@@ -8,9 +8,10 @@ public class ThrowExam {
 	public static void main(String[] args) {
 		ThrowExam ts = new ThrowExam();
 		try {
-		arrayMethod();
-		} catch(ArrayIndexOutOfBoundsException ae) {
-			System.out.println("배열 예외 발생");
+			arrayMethod();
+		} catch(ArrayIndexOutOfBoundsException e){
+			
 		}
+		System.out.println("내가 갑이다.");
 	}
 }
